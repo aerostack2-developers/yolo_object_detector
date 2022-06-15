4caa255269b73d9eec0f93c1af1fa551ef632e8f
Miguel <miferco97@gmail.com>
2022-06-15 14:47:13 +0200

    first commit

---
+++ openrobotics_darknet_ros/build/linux-debug/CMakeFiles/openrobotics_darknet_ros.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/miguel/aerostack2_ws/src/aerostack2/stack_devel/openrobotics_darknet_ros/src/detector_network.cpp" "/home/miguel/aerostack2_ws/src/aerostack2/stack_devel/openrobotics_darknet_ros/build/linux-debug/CMakeFiles/openrobotics_darknet_ros.dir/src/detector_network.cpp.o"
+  "/home/miguel/aerostack2_ws/src/aerostack2/stack_devel/openrobotics_darknet_ros/src/parse.cpp" "/home/miguel/aerostack2_ws/src/aerostack2/stack_devel/openrobotics_darknet_ros/build/linux-debug/CMakeFiles/openrobotics_darknet_ros.dir/src/parse.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DARKNET_ROS_BUILDING_DLL"
+  "GPU=1"
+  "RCUTILS_ENABLE_FAULT_INJECTION"
+  "openrobotics_darknet_ros_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../include"
+  "/opt/ros/galactic/include"
+  "/usr/include/opencv4"
+  "/home/miguel/aerostack2_ws/install/darknet_vendor/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ yolo_object_detector/launch/yolo_object_detector_launch.py
@@ -0,0 +1,63 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.substitutions import LaunchConfiguration, EnvironmentVariable, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+import yaml, os
+
+def get_yolo_params(context, *args, **kwargs):
+
+    config = LaunchConfiguration('config').perform(context)
+    tmp_filename = '/tmp/yolo_params.yaml'
+
+    path_dict = {}
+    with open(config, "r") as f:
+        config_params = yaml.safe_load(f)
+    try:
+        path_dict = {   'config_path' : config_params["/**"]["ros__parameters"]["network"]['config'],
+                        'weight_path' : config_params["/**"]["ros__parameters"]["network"]['weights'],
+                        'class_name_path' : config_params["/**"]["ros__parameters"]["network"]['class_names']}
+    except:
+        raise Exception("Error in yolo_object_detector_launch.py: yolo_params.yaml is not properly formatted")
+
+    for key, value in path_dict.items():
+        if value is None:
+            raise ValueError("No value found for {}".format(value))
+        if not value.startswith('/'):
+            print("Warning: {} is not an absolute path".format(value))
+            path_dict[key] = os.path.join(os.path.dirname(config), value)
+
+    with open( tmp_filename , 'w') as f:
+        config_params["/**"]["ros__parameters"]["network"]['config'] = path_dict['config_path']
+        config_params["/**"]["ros__parameters"]["network"]['weights'] = path_dict['weight_path']
+        config_params["/**"]["ros__parameters"]["network"]['class_names'] = path_dict['class_name_path']
+        yaml.dump(config_params, f)
+
+    node = Node(
+        package='openrobotics_darknet_ros',
+        executable='detector_node',
+        namespace=LaunchConfiguration('drone_id'),
+        parameters=[tmp_filename],
+        remappings=[('detector_node/images', 'sensor_measurement/front_camera')],
+        output='screen',
+        emulate_tty=True
+    )
+
+    return [node]
+
+
+def generate_launch_description():
+    config = PathJoinSubstitution([
+        FindPackageShare('yolo_object_detector'),
+        'config', 'darknet_params.yaml'
+    ])
+
+    ld = LaunchDescription([
+        DeclareLaunchArgument('drone_id', default_value=EnvironmentVariable('AEROSTACK2_SIMULATION_DRONE_ID')),
+        DeclareLaunchArgument('config', default_value=config),
+        OpaqueFunction(function=get_yolo_params)
+    ])
+
+    return ld
+
