540c7d800b7c2089705660b10a6ddd7a2fec7df8
Miguel <miferco97@gmail.com>
2022-06-15 14:49:54 +0200

    clean build

--- openrobotics_darknet_ros/build/linux-debug/CMakeFiles/openrobotics_darknet_ros_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/openrobotics_darknet_ros_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/openrobotics_darknet_ros_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- openrobotics_darknet_ros/build/linux-debug/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /opt/ros/galactic/src/gtest_vendor
-# Build directory: /home/miguel/aerostack2_ws/src/aerostack2/stack_devel/openrobotics_darknet_ros/build/linux-debug/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
